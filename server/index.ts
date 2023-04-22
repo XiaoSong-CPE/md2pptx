75f8e13581c1bfc54e33035dabff838f6b9769b9
Karsten Zhou <1339688323@qq.com>
2023-04-22 17:51:04 +0800

    update server file for compatibility

--- server/index.ts
+++ server/index.ts
@@ -2,49 +2,21 @@ import express from 'express'
 import fs from 'fs'
 import { exec } from 'child_process'
 import http from 'http'
-import https from 'https'
 import path from 'path'
 import pptxgen from 'pptxgenjs'
-import cors from 'cors'
-import vhost from 'vhost'
 
-// 设置https
-let options = {}
-let dev = true
-try {
-  options = {
-    key: fs.readFileSync('C:\\Certbot\\live\\cnection.cn\\privkey.pem'),
-    cert: fs.readFileSync('C:\\Certbot\\live\\cnection.cn\\fullchain.pem')
-  }
-  dev = false
-} catch (error) {
-  console.log('证书加载失败，使用测试模式')
-}
-
-// 设置协议
-const server = (dev ? http : https) as typeof http
-const protocol = dev ? 'http' : 'https'
-const port = dev ? 80 : 443
-const host = dev ? 'localhost' : 'ppt.cnection.cn'
+// 如果直接运行，就是开发环境。如果被调用，就是生产环境
+const dev = require.main === module
 
 // 初始化 express
 const app = express()
-// 设置 CORS
-app.use(
-  cors({
-    origin: dev? '*' : 'https://ppt.cnection.cn',
-  })
-)
-// 设置子域名
-app.use(
-  vhost(host, (req, res, next) => {
-    next()
-  })
-)
+
 // 启动静态文件服务
 app.use(express.static(path.join(__dirname, '../dist')))
 // 解析API文本
 app.use(express.text())
+// 解析json
+app.use(express.json())
 // API 服务
 app.post('/', (req, res) => {
   const timestamp = Date.now()
@@ -86,7 +58,12 @@ app.post('/', (req, res) => {
   )
 })
 
-// 启动服务
-server.createServer(options, app).listen(port, () => {
-  console.log(`PPT server listening at ${protocol}://${host}:${port}`)
-})
+if (dev) {
+  // 开发环境
+  http.createServer(app).listen(80, () => {
+    console.log(`PPT server listening at http://localhost`)
+  })
+}
+
+// 导出app
+export { app }
